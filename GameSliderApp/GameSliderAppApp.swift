a39ea9f269c661eaf459a34151455cfebc8819bf
Zaki <menza.zm@gmail.com>
2023-06-21 12:35:34 +0600

    Initial Commit

---
+++ GameSliderApp/GameSliderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameSliderAppApp.swift
+//  GameSliderApp
+//
+//  Created by Zaki on 21.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameSliderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
